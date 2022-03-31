4010a4bca75f90d5cb772153e1c84708d57331c2
Markus Ineichen <markus.ineichen1@sluz.ch>
2022-03-31 13:54:49 +0200

    Initialisierung

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Investment inv = new Investment(300.00);
+            inv.SetInvestmentStrategy(new CryptoInvestment());
+            inv.TalkToBanlEmployee();
+            inv.MakeInvestment();
+
+            Console.ReadKey();
+        }
+    }
+}
